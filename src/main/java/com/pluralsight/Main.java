020734b71d7f8116abb1077ca48bf44548e6b891
wasidc <wasid.chowdhury@icloud.com>
2025-10-01 09:26:45 -0400

    commit

---
+++ src/main/java/com/pluralsight/Main.java
@@ -0,0 +1,66 @@
+package com.pluralsight;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+       /*  String stringNumber = "111";
+
+        int parsedNumber = Integer.parseInt(stringNumber);
+
+        int result = parsedNumber + 1;
+        System.out.println(result);
+
+        */
+
+      /*  String userInput = "2002-10-17"; // YYYY-MM-DD
+
+        LocalDate birthday = LocalDate.parse(userInput);
+        System.out.println(birthday);
+
+        System.out.println("==================================");
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter the date you want (MM/dd/yyyy): ");
+        String dateInputFromUser = scanner.nextLine();
+
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MM/dd/yyyy");
+        LocalDate userDate = LocalDate.parse(dateInputFromUser, formatter);
+
+        System.out.println(userDate);
+
+       */
+
+      /*  StringBuilder skills = new StringBuilder();
+
+        skills.append("Git, ");
+        skills.append("HTML, ");
+        skills.append("and Bootstrap\n");
+        skills.append("ES6, ");
+        skills.append("REST APIs, ");
+        skills.append("Java\n");
+        skills.append("How many cars you want to buy?");
+
+        Scanner scanner = new Scanner(System.in);
+        skills.append("How many cars do you want to buy?");
+        int numberofCars = scanner.nextInt();
+
+        skills.append(" ").append(numberofCars);
+
+        String mySkills = skills.toString();
+        System.out.println(mySkills);
+
+
+        String mySkills = skills.toString();
+        System.out.println(mySkills);
+
+       */
+
+
+    }
+}
+
+
